eea31a89c72273857a7b31577afa5c762ec0642b
Igor Dias <106784522+IgorDias1998@users.noreply.github.com>
2024-03-10 12:49:04 -0300

    Solution todas_palavras_como_titulo.cs

--- todas_palavras_como_titulo.cs
+++ todas_palavras_como_titulo.cs
@@ -1,1 +1,17 @@
+using System.Globalization;
 
+public static class PhraseCase
+{
+  public static string ToPhraseCase(this string phrase)
+  {
+    TextInfo textInfo = new CultureInfo("pt-BR", false).TextInfo;
+    return textInfo.ToTitleCase(phrase);
+  }
+}
+
+/* Exemplo:
+
+Futuros desenvolvedores de software
+Futuros Desenvolvedores De Software
+
+*/
