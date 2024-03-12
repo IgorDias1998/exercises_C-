dc316d0ef98da72bb59cab17f68fbc80bf3c41f0
Igor Dias <106784522+IgorDias1998@users.noreply.github.com>
2024-03-12 08:53:09 -0300

    Solutiion count_sheep.cs

--- count_sheep.cs
+++ count_sheep.cs
@@ -1,1 +1,26 @@
+/* 
+--> Count sheep <--
+Ex: n = 3 result -> 1 sheep...2 sheep...3 sheep
+Ex: n = 5 result -> 1 sheep...2 sheep...3 sheep...4 sheep...5 sheep...
+*/
 
+public static string CountSheep(int n)
+{
+        string frase = string.Empty;
+
+        for (int i = 1; i <= n; i++)
+        {
+            frase += $"{i} sheep...";
+        }
+        return frase;
+}
+
+//Test
+static void Main(string[] args)
+{
+        Console.WriteLine(CountSheep(3));
+        Console.WriteLine(CountSheep(0));
+        Console.WriteLine(CountSheep(1));
+        Console.WriteLine(CountSheep(5));
+        Console.ReadKey();
+}
